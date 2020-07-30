4459aa2fb3609442e68ecee01a1940dc8f92fa53
liuyang9609 <ly_0913@foxmail.com>
2020-07-30 18:43:14 +0800

    update

--- kitti_cloud_generator/kcg_function.h
+++ kitti_cloud_generator/kcg_function.h
@@ -6,13 +6,16 @@
 #include "opencv2/imgcodecs.hpp"
 #include "opencv2/highgui.hpp"
 #include "opencv2/core/utility.hpp"
+
 #include <pcl/io/pcd_io.h>
 #include <pcl/console/parse.h>
 #include <pcl/visualization/pcl_visualizer.h>
+
 #include <iostream>
 #include <string>
 
 using namespace cv;
+using namespace pcl;
 
 
 class KITTICloudGenerator
@@ -22,16 +25,18 @@ private:
 
 public:
 	KITTICloudGenerator();
-	void disparityMapGenerator(Mat leftImage, Mat rightImage,int minDisparity=0,
-		int blockSize=11,int disp12MaxDiff=1,int preFilterCap=63, int uniquenessRatio=15,int speckleWindowSize=200,int speckleRange=2); 
-	void cloudGenerator(Mat leftImage, Mat rightImage,Mat Q,int minDisparity=0,
-		int blockSize=11,int disp12MaxDiff=1,int preFilterCap=63, int uniquenessRatio=15,int speckleWindowSize=200,int speckleRange=2); 
+	void disparityMapGenerator(Mat leftImage, Mat rightImage, int minDisparity = 0,
+		int blockSize = 11, int disp12MaxDiff = 1, int preFilterCap = 63, int uniquenessRatio = 15, int speckleWindowSize = 200, int speckleRange = 2);
+	void cloudGenerator(Mat leftImage, Mat rightImage, Mat Q, int minDisparity = 0,
+		int blockSize = 11, int disp12MaxDiff = 1, int preFilterCap = 63, int uniquenessRatio = 15, int speckleWindowSize = 200, int speckleRange = 2);
 	Mat getDepthMap();
 	//void cloudAlign(cloud1,cloud2,Q1,Q2)
-    ~KITTICloudGenerator();
+	~KITTICloudGenerator();
 
 
 };
+
+void viewerOneOff(visualization::PCLVisualizer& viewer);
 
 #endif //KITTICLOUDGENERATOR_H
 
--- kitti_cloud_generator/kitti_cloud_generator.cpp
+++ kitti_cloud_generator/kitti_cloud_generator.cpp
@@ -1,30 +1,33 @@
 ﻿//export LD_LIBRARY_PATH=/home/vanessadantas/Downloads/opencv-3.4.1/Build/lib
 #include <iostream>
 #include <opencv2/opencv.hpp>
+#include <pcl/io/io.h>  
+#include <pcl/io/pcd_io.h>  
+#include <pcl/visualization/cloud_viewer.h>
+
 #include "kcg_function.h"
+
 
 using namespace std;
 using namespace cv;
+using namespace pcl;
 
 
 int main()
 {
-    KITTICloudGenerator kcg;
-    Mat leftimage, rightimage;
-    Mat Q = (cv::Mat_<float>(4, 4) << 1, 0, 0, -607.1928,
-        0, 1, 0, -185.2157,
-        0, 0, 0, 718.856,
-        0, 0, 1 / 0.54, 0);
+	KITTICloudGenerator kcg;
+	Mat leftimage, rightimage;
+	Mat Q = (cv::Mat_<float>(4, 4) << 1, 0, 0, -607.1928,
+		0, 1, 0, -185.2157,
+		0, 0, 0, 718.856,
+		0, 0, 1 / 0.54, 0);
 
+	leftimage = imread("000048L.png");
+	rightimage = imread("000048R.png");
 
-    leftimage = imread("000000L.png");
-    rightimage = imread("000000R.png");
+	kcg.cloudGenerator(leftimage, rightimage, Q);
+	//kcg.disparityMapGenerator(leftimage,rightimage);
 
-    kcg.cloudGenerator(leftimage, rightimage, Q);
-    //kcg.disparityMapGenerator(leftimage,rightimage);
-
-
-
-    return 0;
+	return 0;
 }
 
