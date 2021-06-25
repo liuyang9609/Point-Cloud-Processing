582bb343256134b7ce8593d9dc769e7900a4fb77
Liu Yang <ly_0913@foxmail.com>
2021-06-25 20:39:53 +0800

    update

---
+++ 04_pcl_view/pcl_view.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>  
+
+#include <pcl/io/io.h>  
+#include <pcl/io/pcd_io.h>  
+#include <pcl/visualization/cloud_viewer.h>
+
+using namespace pcl;
+
+void viewerOneOff(visualization::PCLVisualizer& viewer)
+{
+	viewer.setBackgroundColor(0.0, 0.0, 0.0);
+}
+
+int main()
+{
+	pcl::PointCloud<pcl::PointXYZ>::Ptr cloud(new pcl::PointCloud<pcl::PointXYZ>);
+
+	if (pcl::io::loadPCDFile<pcl::PointXYZ>("Modelpcd.pcd", *cloud) == -1)//*打开点云文件
+	{
+		PCL_ERROR("Couldn't read file.\n");
+		return(-1);
+	}
+	
+	visualization::CloudViewer viewer("Cloud Viewer");
+	viewer.showCloud(cloud);
+	viewer.runOnVisualizationThreadOnce(viewerOneOff);
+
+	while (!viewer.wasStopped()) {
+	}
+
+	return 0;
+}
+
+
---
+++ 06_Centroid/06_Centroid.cpp
@@ -0,0 +1,22 @@
+﻿#include <pcl/io/pcd_io.h>
+#include <pcl/common/centroid.h>
+#include <iostream>
+
+int main( )
+{
+	pcl::PointCloud<pcl::PointXYZ>::Ptr cloud(new pcl::PointCloud<pcl::PointXYZ>);
+
+	if (pcl::io::loadPCDFile<pcl::PointXYZ>("Modelpcd.pcd", *cloud) == -1)//*打开点云文件
+	{
+		PCL_ERROR("Couldn't read file.\n");
+		return(-1);
+	}
+
+	Eigen::Vector4f centroid;
+	pcl::compute3DCentroid(*cloud, centroid);
+	std::cout << "the xyz coordinates of the centorid are ("
+		<< centroid[0] << ","
+		<< centroid[1] << ","
+		<< centroid[2] << ")" << std::endl;
+	return 0;
+}
